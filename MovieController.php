261b6c15cc9adf771fa57f4d459013ff6b6f48bc
Boudra Tristan <91051365+Tristan-Boudra@users.noreply.github.com>
2022-12-05 16:21:00 +0100

    [WIP] feat(MovieControler): add function roles

---
+++ MovieController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    public function show($id) {
+        $movie = Movie::find($id);
+        return view('movies.show', ['movies' => $movie]);
+    }
+
+    public function movies(){
+        $request = request();
+        $page = $request->input('page');
+        $order_by = $request->input('order_by');
+        $order = $request->input('order');
+
+        $query = movie::query();
+
+        if(($order_by == 'startYear' || $order_by == 'averageRating') && ($order == 'asc' || $order == 'desc')){
+            $query->orderBy($order_by, $order);
+        }
+        $movies = $query->paginate(20);
+        return view('movies', ['movies' => $movies, 'page' => $page, 'order_by' => $order_by, 'order' => $order]);
+    }
+
+    public function random(){
+        $movies = Movie::inRandomOrder()->whereNotNull('poster')->limit(10)->get();
+        return view('movies.random', ['movies' => $movies]);
+    }
+
+    public function roles()
+    {
+        // return $this->belongsToMany(MovieController::class, movies_genres);
+    }
+}
+?>
